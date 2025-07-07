18726d75a42dae487e0aedd56651bf3f8e994dda
pranav-d33 <dhiranpranav72@gmail.com>
2025-07-07 14:53:03 +0530

    small change

--- lakshya_agent/tools/mcp_loader.py
+++ lakshya_agent/tools/mcp_loader.py
@@ -1,7 +1,8 @@
 import json
+import os
 
 def load_mcp_snapshot():
-    file_path = r"C:\Users\Admin\Project\google_aah_project\mcp_snapshot.json"
+    file_path = os.path.join("lakshya_agent", "mcp_snapshot.json")
     try:
         with open(file_path, "r", encoding="utf-8") as f:
             return json.load(f)
